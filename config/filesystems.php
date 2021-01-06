f0c30904a6e6328694cbc6dfba88d559db8bd835
laravel <kushiy@mail.ru>
2021-01-06 19:13:59 +0300

    Laravel

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,7 +42,7 @@ return [
             'visibility' => 'public',
             'links' => [
                 public_path('storage') => storage_path('app/public'),
-                public_path('images') => storage_path('app/images'),
+                /*public_path('images') => storage_path('app/images'),*/
             ],
         ],
 
